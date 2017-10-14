14dbb0e6e6a5150bb06db500ec46c0bca59f6d9b
Eilidh Southren <eilidhsouthren@gmail.com>
2017-10-14 09:45:45 +0200

    basic formatting of output data. 'this is a little... this is a lot...

--- test.py
+++ test.py
@@ -12,8 +12,51 @@ tone_analyzer = ToneAnalyzerV3(
 
 print("\ntone_chat() example 1:\n")
 
-             
+
+# Sentences = False: reads whole string. True, each sentence is analysed
+# Content Type: can be text/plain or text/html
+
+user_input = input('What text would you like to analyse?')
 
 print("\ntone() example 1:\n")
-print(json.dumps(tone_analyzer.tone(text='I am very happy. It is a good day.'),
-                 indent=2))
+return_data = json.dumps(tone_analyzer.tone(text=user_input, sentences=False),
+                 indent=2)
+
+return_data_dict = json.loads(return_data)
+'''
+# Emotion tones:
+ANGER: return_data_dict['document_tone']['tone_categories'][0]['tones'][0]   #  add ['score'] for float score
+DISGUST: return_data_dict['document_tone']['tone_categories'][0]['tones'][1]['score']
+FEAR: return_data_dict['document_tone']['tone_categories'][0]['tones'][2]
+JOY: '[3]'
+SADNESS: [4]
+ANALYTICAL: return_data_dict['document_tone']['tone_categories'][1]['tones'][0]
+CONFIDENT: [1]
+TENTATIVE: [2]
+
+Scoring System: > 0.5 = 'likely', > 0.75 = 'highly likely'
+'''
+#print(return_data_dict['document_tone']['tone_categories'][1]['tones'][2])
+
+emotions_dict = { 'anger' : return_data_dict['document_tone']['tone_categories'][0]['tones'][0]['score'],
+                  'disgust' : return_data_dict['document_tone']['tone_categories'][0]['tones'][1]['score'],
+                  'fear' : return_data_dict['document_tone']['tone_categories'][0]['tones'][2]['score'],
+                  'joy' : return_data_dict['document_tone']['tone_categories'][0]['tones'][3]['score'],
+                  'sadness' : return_data_dict['document_tone']['tone_categories'][0]['tones'][4]['score'],
+                  'analytical' : return_data_dict['document_tone']['tone_categories'][1]['tones'][0]['score'],
+                  'confidence' : return_data_dict['document_tone']['tone_categories'][1]['tones'][1]['score'],
+                  'tentativeness' : return_data_dict['document_tone']['tone_categories'][1]['tones'][2]['score']}
+
+print('User Input: {}\n\n'.format(user_input))
+output = 'Nothing much to see here'
+
+for key in emotions_dict:
+    if emotions_dict[key] >= 0.5:
+        if emotions_dict[key] >= 0.75:
+            output = ('This text has a lot of {} in it.\n'.format(key))
+        output = ('This text has a little {} in it.\n'.format(key))
+
+print(output)
+
+
+
