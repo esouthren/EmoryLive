8201c9a23e9f610cdc974476ec50b89ee776c8ec
Eilidh Southren <eilidhsouthren@gmail.com>
2017-10-14 11:28:37 +0200

    test confirmed; analysis works compared to IBM test data

--- test.py
+++ test.py
@@ -20,24 +20,21 @@ user_input = "I think I'm a super happy sentence! It's just swell to meet you an
 return_data = json.dumps(tone_analyzer.tone(text=user_input),
                  indent=2)
 '''
+user_input2 = "I hate these new features On #ThisPhone after the update.I hate #ThisPhoneCompany products, " \
+              "you'd have to torture me to get me to use #ThisPhone. The emojis in #ThisPhone are stupid." \
+              "#ThisPhone is a useless, stupid waste of money.#ThisPhone is the worst phone I've ever had - ever 😠." \
+              "#ThisPhone another ripoff, lost all respect SHAME." \
+              "I'm worried my #ThisPhone is going to overheat like my brother's did." \
+              "#ThisPhoneCompany really let me down... my new phone won't even turn on."
 
+user_input3 = "As I guess you've been told, the physics department has set up a new mentoring buddy scheme this year, and I've been assigned as your buddy/mentor/massively experienced PhD guru, so I just wanted to reach out and say hi! A bit about me; I did my undergrad degree in Manchester and moved to Warwick in 2015. I'm just starting the 3rd year of my PhD in the plasma physics group (the CFSA), studying how the solar wind plasma has changed over the last 20-50 years. I also demonstrate 2nd year Python programming workshops, and assist in teaching 1st year library skills. To be honest, I haven't received much information about this mentoring scheme, so I'm not sure what they're expecting me to do. Obviously if you have any problems or you're not sure who to contact about something, feel free to drop me an email or come see me (my office is PS117, I'm usually here 9 til 5 or 6). I'm always up for tea/coffee if you just want to chat to someone outside of your group (on that note, I highly recommend Research Refresh, Thursday mornings 10.30 til 12 in the Wolfson Research Exchange, 3rd floor library - free coffee and cake is always good!) If you're perfectly happy and don't want mentoring, that's good too, it's entirely up to you!"
 # with open(join(dirname(__file__), 'tone.json')) as tone_json: // for reading from a json file
 
-return_data_dict = tone_analyzer.tone(user_input,
+return_data_dict = tone_analyzer.tone(user_input2,
     content_type='text/plain')
 print(return_data_dict)
 
 '''
-# Emotion tones:
-ANGER: return_data_dict['document_tone']['tone_categories'][0]['tones'][0]   #  add ['score'] for float score
-DISGUST: return_data_dict['document_tone']['tone_categories'][0]['tones'][1]['score']
-FEAR: return_data_dict['document_tone']['tone_categories'][0]['tones'][2]
-JOY: '[3]'
-SADNESS: [4]
-ANALYTICAL: return_data_dict['document_tone']['tone_categories'][1]['tones'][0]
-CONFIDENT: [1]
-TENTATIVE: [2]
-
 Scoring System: > 0.5 = 'likely', > 0.75 = 'highly likely'
 '''
 #print(return_data_dict['document_tone']['tone_categories'][1]['tones'][2])
@@ -59,7 +56,7 @@ emotions_dict = { 'anger' : return_data_dict['document_tone']['tone_categories']
                   }
 
 
-print('User Input: {}\n\n'.format(user_input))
+#print('User Input: {}\n\n'.format(user_input))
 output = 'Nothing much to see here'
 high_score = 0
 high_val = 'fear'
@@ -74,10 +71,12 @@ for key in emotions_dict:
         high_score = emotions_dict[key]
         high_val = key
 
-    if emotions_dict[key] >= 0.5:
-        if emotions_dict[key] >= 0.75:
-            print('This text has a lot of {} in it.'.format(key))
-        else: print('This text has a little {} in it.'.format(key))
+    if 0.75 > emotions_dict[key] >= 0.5:
+        print('This text has a little {} in it.'.format(key))
+
+    if emotions_dict[key] >= 0.75:
+        print('This text has a lot of {} in it.'.format(key))
+
 
 if high_score < 0.5:
     print("There's a glimmer of {} in it. ({})".format(high_val, high_score))
