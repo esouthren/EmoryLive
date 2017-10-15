5bc1c360f0eb2704793315cbdc966a89d5df7d2b
Eilidh Southren <eilidhsouthren@gmail.com>
2017-10-15 02:14:29 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- controller.js
+++ controller.js
@@ -7,12 +7,40 @@ var textMachine = angular.module("textMachine", ['ngAnimate']);
 //define controller for application
 textMachine.controller("textMachineController", function($scope, $http) {
                                                      
-    $scope.response = "API response!";
-    $scope.boop = "boop"
-	
-    $scope.callApi = function(){
-        
+    $scope.response = "";
+    
+    $scope.first = {};
+    $scope.second = {};
+    $scope.third = {};
 
+    $scope.entries_cache = $scope.entries;
+    
+    $scope.entries = ["Welcome to our app. I'm a test sentence. It's really great to meet you! Have a nice day :D",
+                     "This is the second array entry! Holy moly!"];
+    
+    $scope.addEntry = function() {
+        $scope.entries_cache = $scope.entries;
+        ($scope.entries).push(" ");
+    }
+	
+    $scope.removeAll = function() {
+        $scope.entries_cache = $scope.entries;
+        for (e in $scope.entries) {
+            e = '';
+        }
+    }
+    
+    $scope.woops = function() {
+        $scope.entries = $scope.entries_cache;
+    }
+    
+    
+    $scope.callApi = function(){        
+        
+        for (var e = 0; e < ($scope.entries).length; e++) {
+            $scope.response =  ($scope.response).concat($scope.entries[e]);
+        }
+        
         // aight here we go! Starting API Call
         $http({
             url: 'http://localhost:8080/',
@@ -23,20 +51,43 @@ textMachine.controller("textMachineController", function($scope, $http) {
 	          method: "POST",
               // if successful API call:
             }).success(function(response){
-
-              // hide the loading gif
-             $scope.loading = false;
-              if (!response.data[0]) {
+                $scope.response = response;
+              if (!response[0]) {
                   console.log("nothing here boss");
                   // display error image
               }
+            
+            // LET'S GET SOME FACTS! //
+            
+            // This is the worst possible way of finding the top three values
+            // but give me a break, I've been awake for some time now
+            
+            
+                
+            
+             
+            
         });
-        $scope.boop = "boop boop!";
-    }
-                                    
-    $scope.entries = response.data
+
+    }                                   
+    
                                                  
 });
+/*
+ anger: 0.255687,
+  disgust: 0.07021,
+  fear: 0.091376,
+  joy: 0.014206,
+  sadness: 0.695304,
+  analysis: 0,
+  confidence: 0,
+  tentativeness: 0,
+  openness: 0.246078,
+  conscientiousness: 0.275057,
+  extraversion: 0.535544,
+  agreeableness: 0.595745,
+  'emotional range': 0.190936
+  */
                                                 
 
        
--- node_stuff.js
+++ node_stuff.js
@@ -32,6 +32,24 @@ app.post("/", function(user_request, response_to_user){
         console.log('error:', error);
       }
       else {
+          // Parse Data
+          watson_data = { 'anger' : watson_data['document_tone']['tone_categories'][0]['tones'][0]['score'],
+                  'disgust' : watson_data['document_tone']['tone_categories'][0]['tones'][1]['score'],
+                  'fear' : watson_data['document_tone']['tone_categories'][0]['tones'][2]['score'],
+                  'joy' : watson_data['document_tone']['tone_categories'][0]['tones'][3]['score'],
+                  'sadness' : watson_data['document_tone']['tone_categories'][0]['tones'][4]['score'],
+                  'analysis' : watson_data['document_tone']['tone_categories'][1]['tones'][0]['score'],
+                  'confidence' : watson_data['document_tone']['tone_categories'][1]['tones'][1]['score'],
+                  'tentativeness' : watson_data['document_tone']['tone_categories'][1]['tones'][2]['score'],
+                  'openness' : watson_data['document_tone']['tone_categories'][2]['tones'][0]['score'],
+                  'conscientiousness' : watson_data['document_tone']['tone_categories'][2]['tones'][1]['score'],
+                  'extraversion' : watson_data['document_tone']['tone_categories'][2]['tones'][2]['score'],
+                  'agreeableness' : watson_data['document_tone']['tone_categories'][2]['tones'][3]['score'],
+                  'emotional range' : watson_data['document_tone']['tone_categories'][2]['tones'][4]['score'],
+                  };
+          console.log(watson_data);
+          
+          
           response_to_user.send(watson_data);
          // var tones = response.document_tone.tone_categories.tones
           var tones = watson_data;
